adfc158c65ca6ea53852a339f93e1a99d76c3b37
c0der.br1t <c0der.br1t@gmail.com>
2019-11-12 14:57:13 -0600

    4 tests passing, including example. Finished 'processSum()'

--- index.js
+++ index.js
@@ -89,8 +89,12 @@ function processLastItem(stringList, callback) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
+function processSum(numberList, callback) {
   /* CODE HERE */
+  const total = numberList.reduce(function(accumulator, currentValue){
+    return accumulator + currentValue;
+  }, 0);
+  return callback(total);
 }
 
 /**
